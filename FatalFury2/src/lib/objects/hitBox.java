5552d074fdad56176189732b441ed785b179037b
sisques <vsisquesc@gmail.com>
2020-03-09 18:43:29 +0100

    HITBOXES

---
+++ FatalFury2/src/lib/objects/hitBox.java
@@ -0,0 +1,77 @@
+package lib.objects;
+
+import java.awt.*;
+
+public class hitBox {
+
+    private int width, height;
+    private int x, y;
+    // if !hitbox -> its a hurtbox
+    private Boolean hitbox;
+
+    public hitBox(int originX, int originY, int width, int height, Boolean hitbox) {
+        this.width = width;
+        this.height = height;
+        this.x = originX;
+        this.y = originY;
+        this.hitbox = hitbox;
+    }
+
+    public void updateHitBox(int newX, int newY, int newWidth, int newHeight) {
+        this.x = newX;
+        this.y = newY;
+        this.width = newWidth;
+        this.height = newHeight;
+    }
+
+
+
+    public Boolean collides(hitBox other) {
+        if (this.x < other.x + other.width &&
+                this.x + this.width > other.x &&
+                this.y < other.y + other.height &&
+                this.y + this.height > other.y) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public void drawHitBox(Graphics g) {
+        if (this.hitbox) {
+            g.setColor(Color.RED);
+        } else {
+            g.setColor(Color.BLUE);
+        }
+        g.drawRect(this.x, this.y, this.width, this.height);
+    }
+
+
+    public Boolean isHitbox() {
+        return hitbox;
+    }
+
+    public Boolean isHurtbox() {
+        return !hitbox;
+    }
+
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    }
