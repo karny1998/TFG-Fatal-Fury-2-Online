ba0b4bf8ac3af8d2806d39a7bc433480d5998f36
sisques <vsisquesc@gmail.com>
2020-05-08 21:13:47 +0200

    NAME

--- FatalFury2/src/lib/objects/ask_for_name.java
+++ FatalFury2/src/lib/objects/ask_for_name.java
@@ -3,29 +3,66 @@ package lib.objects;
 import lib.Enums.Item_Type;
 import lib.input.controlListener;
 import lib.sound.audio_manager;
-import lib.sound.fight_audio;
 import lib.sound.menu_audio;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
 
 public class ask_for_name {
-    private String name = "";
-    private animation anim;
-    private Font f = new Font("Orbitron", Font.ROMAN_BASELINE, 60);
-    private long timeReference = System.currentTimeMillis();
+    private Font f;
+
+    {
+        try {
+            f = Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("/files/fonts/m04b.TTF")).deriveFont(100f);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private URL imgPath_1 = this.getClass().getResource("/assets/sprites/menu/rank_register/base.png");
+    private URL imgPath_2 = this.getClass().getResource("/assets/sprites/menu/rank_register/ar.png");
+    private URL imgPath_3 = this.getClass().getResource("/assets/sprites/menu/rank_register/ab.png");
+    private screenObject fondo = new screenObject(0, 0,  1280, 720, new ImageIcon(imgPath_1).getImage(), Item_Type.MENU);
+
+    private int[] xFlechas = {465,615,765};
+    private int yAr = 200;
+    private int yAb = 400;
+
+
+    private long referenceTime;
+
+    private char[] valores = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z','-','.'};
+
+    private int[] resultado = {0,0,0};
+
+    public int actual;
+
+    private screenObject ar;
+    private screenObject ab;
+
+    private boolean done;
 
     public  ask_for_name(){
-        timeReference = System.currentTimeMillis();
-        loadAnim();
+        actual = 0;
+        done = false;
+        ar = new screenObject(xFlechas[actual], yAr,  54, 36, new ImageIcon(imgPath_2).getImage(), Item_Type.P1_SELECT);
+        ab = new screenObject(xFlechas[actual], yAb,  54, 36, new ImageIcon(imgPath_3).getImage(), Item_Type.P2_SELECT);referenceTime = System.currentTimeMillis();
         String path = System.getProperty("user.dir") + "/.files/last_name.txt";
         try {
             File f = new File(path);
             BufferedReader b = new BufferedReader(new FileReader(f));
             String aux = "";
             if ((aux = b.readLine()) != null) {
-                name = aux;
+                resultado[0] = toValue(aux.charAt(0));
+                resultado[1] = toValue(aux.charAt(1));
+                resultado[2] = toValue(aux.charAt(2));
             }
             b.close();
         }
@@ -34,50 +71,74 @@ public class ask_for_name {
         }
     }
 
-    private void loadAnim(){
-        String path = "/assets/sprites/menu/rank_register/";
-        anim = new animation();
-        anim.setHasEnd(false);
-        screenObject s = new screenObject(191, 186,  897, 347, new ImageIcon( this.getClass().getResource(path  + "rank_register_1.png")).getImage(), Item_Type.MENU);
-        anim.addFrame(s,250.0,0,0);
-        s = new screenObject(191, 186,  897, 347, new ImageIcon( this.getClass().getResource(path  + "rank_register_2.png")).getImage(), Item_Type.MENU);
-        anim.addFrame(s,250.0,0,0);
-        anim.setHasSound(false);
-        anim.setSoundType(fight_audio.voice_indexes.Win);
+
+    private int toValue(char c){
+        for(int i = 0; i < valores.length; i++){
+            if (valores[i] == c){
+                return i;
+            }
+        }
+        return 0;
     }
 
-    public  screenObject getAnimation(){
-        return anim.getFrame(191, 186, 1);
+    public Boolean gestionMenu(Map<Item_Type, screenObject> screenObjects){
+
+        screenObjects.put(Item_Type.MENU, fondo);
+        ar = new screenObject(xFlechas[actual], yAr,  54, 36, new ImageIcon(imgPath_2).getImage(), Item_Type.P1_SELECT);
+        ab = new screenObject(xFlechas[actual], yAb,  54, 36, new ImageIcon(imgPath_3).getImage(), Item_Type.P2_SELECT);
+        screenObjects.put(Item_Type.P1_SELECT, ar);
+        screenObjects.put(Item_Type.P2_SELECT, ab);
+        if(controlListener.getStatus(0, controlListener.ENT_INDEX)){
+            done = true;
+            audio_manager.menu.play(menu_audio.indexes.option_selected);
+        }
+        return done;
     }
 
-    public  void writeName(Graphics2D g){
+    public void writeName(Graphics2D g){
         long current = System.currentTimeMillis();
-        boolean ok = false;
-       if(current - timeReference > 75){
-            timeReference = current;
-            ok = true;
-        }
-        int aux = controlListener.getCurrentKey();
-        char c = (char)aux;
-        boolean isAlphanumeric =  (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') ? true : false;
-        if(ok && name.length() < 10 && aux != -1 && isAlphanumeric) {
-            name += c;
-        }
-        else if(ok && controlListener.isPressed(8) && name.length() > 0){
-            audio_manager.menu.play(menu_audio.indexes.move_cursor);
-            name = name.substring(0, name.length()-1);
+        if(current - referenceTime > 125){
+            if(controlListener.getStatus(0, controlListener.DE_INDEX)){
+                actual = actual + 1 > resultado.length-1 ? resultado.length - 1 :  actual + 1;
+                audio_manager.menu.play(menu_audio.indexes.move_cursor);
+            }
+            else if(controlListener.getStatus(0, controlListener.IZ_INDEX)){
+                actual = actual - 1 < 0 ? 0 :  actual - 1;
+                audio_manager.menu.play(menu_audio.indexes.move_cursor);
+            }
+            else if(controlListener.getStatus(0, controlListener.AR_INDEX)){
+                resultado[actual] = resultado[actual] + 1 > valores.length-1 ? 0 :  resultado[actual] + 1;
+                audio_manager.menu.play(menu_audio.indexes.move_cursor);
+            }
+            else if(controlListener.getStatus(0, controlListener.AB_INDEX)){
+                resultado[actual] = resultado[actual] - 1 < 0 ? valores.length -1 :  resultado[actual] - 1;
+                audio_manager.menu.play(menu_audio.indexes.move_cursor);
+            }
+
+            referenceTime = current;
         }
 
+
+        int x = 440;
+        int y = 370;
         g.setFont(f);
-        g.setColor(Color.YELLOW);
-        g.drawString(name, 271, 376);
+        for(int i = 0; i < resultado.length; i++){
+            if(i == actual){
+                 g.setColor(new Color(255, 221, 0));
+             } else {
+                g.setColor(new Color(140, 120, 0));
+            }
+            g.drawString(String.valueOf(valores[resultado[i]]), x, y);
+            x+=150;
+        }
     }
 
     public String getName() {
-        return name;
+        String out = "";
+        for(int i = 0; i < resultado.length; i++){
+            out += valores[resultado[i]];
+        }
+        return out;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
