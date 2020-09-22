f54ece1eb9e92d89c59338d3ce201412707b9c0b
karny.sac@gmail.com <rdqvza199819981998s>
2020-09-22 16:47:19 +0200

    Corregidos algunos detalles de la interfaz

--- IA_Analyzer/src/code/main.java
+++ IA_Analyzer/src/code/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args){
         boolean simple = false;
-        stadistics s = new stadistics("q_learning_statsSameVsAleartoryTest.xml");
+        stadistics s = new stadistics("q_learning_statsAleatoryRegresionYes6.xml");
         s.printEfectivity(simple);
         s.printAccumulatedReward(simple);
         s.printStadistics(simple);
